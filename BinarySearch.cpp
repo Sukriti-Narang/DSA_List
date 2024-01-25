204ffdd53be7f91210b1d054b4c6717f804add13
Sukriti-Narang <100237223+Sukriti-Narang@users.noreply.github.com>
2024-01-25 17:46:25 +0530

    Update and rename BinarySearch to BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,4 +1,4 @@
-BINARY SEARCH
+##BINARY SEARCH
 
 1 https://leetcode.com/problems/search-insert-position/description/
 2 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
